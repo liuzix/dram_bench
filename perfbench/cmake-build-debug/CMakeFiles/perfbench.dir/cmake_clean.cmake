421e2642e1010150964c064e3f9d991fd7d4bdcb
Zixiong Liu <lzx@uchicago.edu>
2017-12-16 12:34:15 +0800

    performance benchmarking

---
+++ perfbench/cmake-build-debug/CMakeFiles/perfbench.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/perfbench.dir/main.cpp.o"
+  "perfbench.pdb"
+  "perfbench"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/perfbench.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
